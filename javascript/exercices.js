50d28fab27cc44ef7b2888ea7c715e134019e6e0
mohigh999 <pierre.monier99@gmail.com>
2020-02-04 15:03:17 +0100

    It was hard dude

--- javascript/exercices.js
+++ javascript/exercices.js
@@ -1,16 +1,27 @@
 const anagrammes = (stringA, stringB) => {
-  /**
-   * stringA est égale à stringB si et seulement s'ils partagent les mêmes
-   * caractères alphabétiques dans la même quantité.
-   * La case n'est pas pris en compte
-   *
-   * Exemples :
-   *
-   * anagrams('rail safety', 'fairy tales') === true
-   * anagrams('RAIL! SAFETY!', 'fairy tales') === true
-   * anagrams('Hi there', 'Bye there') === false
-   */
 
+  stringA = stringA.replace(/[^A-Za-z0-9_]/g,"");
+  stringB = stringB.replace(/[^A-Za-z0-9_]/g,"");
+
+  stringA = stringA.toLowerCase();
+  stringB = stringB.toLowerCase();
+
+  stringA = stringA.split('');
+  stringB = stringB.split('');
+
+  stringA = stringA.sort();
+  stringB = stringB.sort();
+
+  stringA = stringA.join('');
+  stringB = stringB.join('');
+
+  console.log(stringA+"    "+stringB);
+
+  if(stringA == stringB){
+    return true;
+  }else{
+    return false;
+  }
 };
 
 
@@ -32,6 +43,26 @@ class Stack {
  * s.pop(); // returns 2
  * s.peek(); // returns 1
  */
+  // tab;
+  constructor(){
+    this.tab = new Array();
+    this.bru = 0;
+  }
+
+  push(x){
+    this.tab.push(x);
+    this.bru++;
+  }
+  pop(){
+    var value = this.tab[this.bru-1];
+    this.tab.splice(this.bru-1, 1);
+    this.bru--;
+    console.log(2)
+    return value;
+  }
+  peek(){
+    return this.tab[this.bru-1];
+  }
 };
 
 
@@ -49,27 +80,76 @@ const fizzBuzz = (n) => {
  * console.log(4)
  * console.log('buzz')
  */
-
+  for(i = 1; i <= n; i++){
+    current = i;
+    if(i % 3 == 0){
+      current = "fizz";
+    }
+    if(i % 5 == 0){
+        current = "buzz";
+      }
+    if(i % 3 == 0 && i % 5 == 0){
+      current = "fizzbuzz";
+    }
+    console.log(current)
+  }
 };
 
 const spirale = (n) => {
-/**
- * Retourne une matrice spirale de taille n x n.
- *
- * Exemples :
- *
- * matrix(2) = [[1, 2],
- *              [4, 3]]
- *
- * matrix(3) = [[1, 2, 3],
- *              [8, 9, 4],
- *              [7, 6, 5]]
- *
- * matrix(4) = [[1,   2,  3, 4],
- *              [12, 13, 14, 5],
- *              [11, 16, 15, 6],
- *              [10,  9,  8, 7]]
- */
+
+  let result = new Array(n).fill().map(() => new Array(n).fill('')); // create empty n x n array
+  let counter = 1;
+  let startCol = 0;
+  let endCol = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+  while (startCol <= endCol && startRow <= endRow) {
+      for (let i = startCol; i <= endCol; i++) {
+          result[startRow][i] = counter;
+          counter++;
+      }
+      startRow++;
+      for (let y = startRow; y <= endRow; y++) {
+          result[y][endCol] = counter;
+          counter++;
+      }
+
+      endCol--;
+
+      for (let i = endCol; i >= startCol; i--) {
+          result[endRow][i] = counter;
+          counter++;
+      }
+
+      endRow--;
+      for (let i = endRow; i >= startRow; i--) {
+          result[i][startCol] = counter;
+          counter++;
+      }
+
+      startCol++;
+
+  }
+
+  return result;
+
+  /**
+   * Retourne une matrice spirale de taille n x n.
+   *
+   * Exemples :
+   *
+   * matrix(2) = [[1, 2],
+   *              [4, 3]]
+   *
+   * matrix(3) = [[1, 2, 3],
+   *              [8, 9, 4],
+   *              [7, 6, 5]]
+   *
+   * matrix(4) = [[1,   2,  3, 4],
+   *              [12, 13, 14, 5],
+   *              [11, 16, 15, 6],
+   *              [10,  9,  8, 7]]
+   */
 
 };
 
@@ -103,7 +183,146 @@ const puissance4 = (grid) => {
  *   [ 2, 2, 1, 1, 2 ]]
  *   ) = 0
  */
+
+function test(dir, y, x, tab){
+  if(dir == 'droite' && y == 0){
+    if(vertical(dir, y, x, tab)){
+      return true;
+    }
+    if(horizontal(dir, y, x, tab)){
+      return true;
+    }
+    if(diagonal(dir, y, x, tab)){
+      return true;
+    }
+  }
 }
+
+
+
+function vertical(dir, y, x, tab) {
+
+  let safe = 0
+  console.log(" VERTICAL : ")
+  console.log(" dir : "+dir+" y : "+y+" x : "+x+" tab : "+tab);
+    if(y == 0){
+      for(i=0; i<4; i++){
+        
+        if(i != 0 && safe != tab[i][x]){
+          console.log("false")
+          return false;
+        }
+        console.log(" x : "+x+" y : "+y);
+        safe = tab[i][x]
+      }
+      console.log("true")
+      return true;
+    } else{
+
+      for(i=4; i>0; i--){
+        
+        if(i != 4 && safe != tab[i][x]){
+          console.log("false")
+          return false;
+        }
+        console.log(" x : "+x+" y : "+y);
+        safe = tab[i][x]
+      }
+      console.log("true")
+      return true;
+    }
+}
+
+function horizontal(dir, y, x, tab){
+
+  let safe = 0;
+  console.log(" HORIZONTAL : ")
+  console.log(" dir : "+dir+" y : "+y+" x : "+x+" tab : "+tab);
+    if(dir == "droite"){
+      for(i = 0; i<4; i++){
+        if(i != 0 && safe != tab[i][x]){
+          console.log("false")
+          return false;
+        }
+        console.log(" x : "+x+" y : "+y);
+        safe = tab[y][i]
+      }
+      console.log("true")
+      return true;
+    } else{
+      for(i = 4; i>0; i--){
+        if(i != 0 && safe != tab[i][x]){
+          console.log("false")
+          return false;
+        }
+        console.log(" x : "+x+" y : "+y);
+        safe = tab[y][i]
+      }
+      console.log("true")
+      return true;
+    }
+}
+function diagonal(dir, y, x, tab){
+  console.log(" DIAGONAL : ")
+  console.log(" dir : "+dir+" y : "+y+" x : "+x+" tab : "+tab);
+    // if(y == 0){
+    //   for
+    // }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  let dir = '';
+  let y = 0;
+  let x = 0;
+  let joueur = 0;
+  let result = 0; 
+  grid.forEach((el) => {
+    el.forEach((sub)=> {
+      if(sub == 1 || sub == 2){
+        joueur = sub;
+        x = el.indexOf(sub);
+        if(el.indexOf(sub) < 2){
+          dir = 'droite';
+        }
+        if(el.indexOf(sub) >= 3){
+          dir = 'gauche';
+        }
+      }
+    })
+    if(test(dir, y, x, grid)){
+      console.log(" OUUUUUUUUUUUUUUUUUUUUIIIIIIIIIIIIIIIIIIIIIIIIII")
+      result = joueur;
+    }else{
+      y++;
+    }
+    console.log("############################################################");
+    return result;
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
 
 module.exports = {
   fizzBuzz,
